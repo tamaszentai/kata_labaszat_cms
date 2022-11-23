d3acd2c1232bbfcc32eb5f0781a160c00049ed66
Tamas Zentai <zentaitamas@gmail.com>
2022-11-23 22:53:07 +0000

    galleryitem added

--- pages/Gallery/index.vue
+++ pages/Gallery/index.vue
@@ -34,28 +34,8 @@
     </form>
     <div>
       <ul class="grid grid-cols-4 gap-4">
-        <li v-for="(image, index) in gallery" :key="index">
-          <img :src="image.imageUrl" :alt="image.description" class="h-80" />
-          <h3>{{ image.description }}</h3>
-          <button
-            class="bg-cms_4 text-cms_black hover:hover:bg-cms_1 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md w-50"
-            @click="deleteImage(image)"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-              />
-            </svg>
-          </button>
+        <li v-for="(galleryItem, index) in gallery" :key="index">
+            <GalleryItem :imageData="galleryItem"/>
         </li>
       </ul>
     </div>
@@ -66,12 +46,13 @@
 import { Component, Vue } from 'nuxt-property-decorator'
 import firebase from 'firebase/compat'
 import DocumentData = firebase.firestore.DocumentData
+import GalleryItem from '~/components/GalleryItem.vue'
 const { v4: uuidv4 } = require('uuid')
 
 @Component({
   // middleware: 'authenticated',
   layout: 'admin',
-  components: {},
+  components: { GalleryItem },
 })
 export default class Gallery extends Vue {
   imageUrl = ''
--- utils/pageTitle.ts
+++ utils/pageTitle.ts
@@ -1,12 +1,3 @@
-export enum pageTitleEnum {
-  "dashboard" = 1,
-  "about" = 2,
-  "services" = 3,
-  "gallery" = 4,
-  "pricelist" = 5,
-  "blog" = 6,
-}
-
 export const pageTitleMap = new Map<string, string>([
   ["dashboard", "Információs pult"],
   ["about", "Magamról"],
@@ -16,11 +7,3 @@ export const pageTitleMap = new Map<string, string>([
   ["blog", "Blog"],
 ]);
 
-export const pageTitleObject = {
-  dashboard: "Információs pult",
-  about: "Magamról",
-  services: "Szolgáltatások",
-  gallery: "Galéria",
-  pricelist: "Árlista",
-  blog: "Blog",
-}
